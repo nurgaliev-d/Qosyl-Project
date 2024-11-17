1ae1977192a1de68c0930d394e8c626b4ae65ec2
Aruzhan <a_akhanayeva@kbtu.kz>
2024-11-18 00:25:59 +0600

    some changes in room

---
+++ base/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# from rooms.models import Room
+# from users.models import User,Topic
+# from chat.models import Message
+ 
+# admin.site.register(User)
+# admin.site.register(Room)
+# admin.site.register(Topic)
+# admin.site.register(Message)
