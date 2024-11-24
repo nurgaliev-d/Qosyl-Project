18a990a0984b44e86d3c9efecf9e453641b1884d
Dias Nurgaliev <nurgalyev.dias@gmail.com>
2024-11-25 01:04:46 +0500

    all users list added

--- users/models.py
+++ users/models.py
@@ -29,7 +29,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
-    avatar = models.ImageField(null=True, default="avatar.svg")
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# app_name = 'users'
+
 urlpatterns = [
     # path('login/', views.loginPage, name="login"),
     path('login/',views.loginPage, name = "login"),
@@ -9,4 +11,5 @@ urlpatterns = [
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('update-user/', views.updateUser, name="update-user"),
     path('topics/', views.topicsPage, name="topics"),
+    path('all/', views.get_all_users, name='all_users'),
 ]
